0011dc4738e15710050c1157fb6aa81a68a430c0
Patryk Pożak <pozak.p@gmail.com>
2023-11-29 00:05:39 +0100

    Task 1 done.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,8 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryList = document.querySelector("#gallery");
+const galleryList = document.querySelector(".gallery");
+let instance;
 const images = galleryItems.map((item) => {
   return `<div class="gallery__item">
     <a class="gallery__link" href="${item.original}">
@@ -12,7 +13,34 @@ const images = galleryItems.map((item) => {
         alt="${item.description}"
       />
     </a>
-    </div>;`;
+    </div>`;
 });
 
 galleryList.insertAdjacentHTML("beforeend", images.join(""));
+
+galleryList.addEventListener("click", onClickGal);
+
+const genBigImg = ({ url, description }) =>
+  `<img class="gallery__image" src="${url}" width="1280" alt="${description}"/>`;
+
+function onClickGal(e) {
+  e.preventDefault();
+
+  const url = e.target.dataset.source;
+  const description = e.target.alt;
+
+  instance = basicLightbox.create(genBigImg({ url, description }), {
+    onClose: () => {
+      galleryList.removeEventListener("keydown", onKDownEsc);
+    },
+  });
+
+  instance.show();
+  galleryList.addEventListener("keydown", onKDownEsc);
+}
+
+const onKDownEsc = (e) => {
+  if (e.key === "Escape" || e.keyCode === 27) {
+    instance.close();
+  }
+};
