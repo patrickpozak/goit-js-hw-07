6718c60cf403c6048c0d20ac9183df8e7a695615
Patryk Pożak <pozak.p@gmail.com>
2023-11-30 23:24:12 +0100

    Task 2 Done. Everything works fine except width and height of opened gallery in SimpleLightbox :/

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const images = galleryItems.map((item) => {
+  return `<li class="gallery__item">
+    <a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+    </li>`;
+});
+
+galleryList.insertAdjacentHTML("beforeend", images.join(""));
+
+galleryList.addEventListener("click", onClickGal);
+
+function onClickGal(e) {
+  e.preventDefault();
+}
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  nav: false,
+  close: false,
+  scrollZoom: false,
+});
